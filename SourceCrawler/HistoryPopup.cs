d09eb1f590ac7b34b0e11921dd0f6fe93768a1b5
Tom Kashin <tkashin@salesforce.com>
2020-02-03 13:33:12 -0500

    Added copyright

--- SourceCrawler/HistoryPopup.cs
+++ SourceCrawler/HistoryPopup.cs
@@ -1,4 +1,11 @@
-﻿using System;
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows.Forms;
